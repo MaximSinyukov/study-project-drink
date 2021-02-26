2792fc175cd7456151bd631b499ed94d1801dc10
Maxim Sinyukov <verero1173@yandex.ru>
2021-02-26 21:10:20 +0300

    feat: создать файловую структуру проекта

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # study-project-drink
+
+<p>
+  <img alt="Status" src="https://img.shields.io/badge/status-unreleased-orange" >
+  <img alt="Author" src="https://img.shields.io/badge/author-MaximSinyukov-blue" />
+</p>
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: { main: './src/page/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'main.js',
+            publicPath: ''
+  },
+  mode: 'development',
+  devServer: {
+    contentBase: path.resolve(__dirname, './build'),
+    compress: true,
+    port: 8080,
+    open: true,
+    writeToDisk: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.(png|svg|jpg|gif|ico|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1
+          }
+        },
+        'postcss-loader'
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'main.css'
+    })
+  ]
+}
